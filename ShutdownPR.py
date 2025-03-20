7afc8cb0971fb969487eddb81b19f02da1e77c14
themanhimeslf <aulua003@osloskolen.no>
2025-03-20 14:00:29 +0100

    yh yh

--- ShutdownPR.py
+++ ShutdownPR.py
@@ -9,9 +9,11 @@ for i in range(10, 0, -1):
     print(f"Shutting down in {i} seconds...")
     time.sleep(1)
 
+print("Just kidding! Your system is safe :)")
+time.sleep(2)
+print("Hehe")
+time.sleep(1)
     # Uncomment this line to actually shut down the system (DANGEROUS)
     # os.system("shutdown /s /t 1")  # Windows shutdown
     # os.system("sudo shutdown -h now")  # Linux shutdown
 
-
-print("Just kidding! Your system is safe :)")
