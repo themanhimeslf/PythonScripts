66bda4f9e3a6f95a75bc8e62cf5c4a0f8fbd985f
themanhimeslf <aulua003@osloskolen.no>
2025-03-26 09:49:26 +0100

    yip

---
+++ listarray.py
@@ -0,0 +1,11 @@
+# bla
+
+
+listpoteter = ["Potet", "Mandelpotet", "fløtegratinertpoteter", "søt potet", "jacket potato"]
+listpoteter[1] = "test"
+print(listpoteter)
+print(listpoteter[4])
+print(", ".join(listpoteter))
+
+for x in listpoteter:
+    print(x)
