6661d645a28afb2988ce8765a9fbbb3136db77aa
themanhimeslf <aulua003@osloskolen.no>
2025-05-02 09:12:35 +0200

    ger

---
+++ Grunnlegende.py
@@ -0,0 +1,4 @@
+# 28/04/25
+
+print("test1", "poteter")
+print()
---
+++ grunnlegende2.py
@@ -0,0 +1,28 @@
+# fornavn = "August"
+# fornavn = input( " navn: " )
+# print("hei", fornavn, "hygg")
+
+# alder = input( "Men hvor gammel er du? " )
+# print("Så du er", alder, "gammel og heter", fornavn)
+
+
+
+# # fornavn = "test"
+# # hilsen = "hei" + fornavn + "heihei"
+# # print(fornavn, hilsen)
+
+
+
+# fornavn = "julenissen"
+# forNavn = "petter"
+
+# fulltnavn = fornavn + fornavn + "eirik"
+
+# print("hei", fulltnavn,"hallo")
+
+navn = ""
+
+print("ost som het karl").
+print("En solfylt dag ble karl veldig sulten. Magen hans romlet.")
+print("Det store kjøleskapet til Karl var  veldig tomt.")
+print("Derfor hentet Karl en stor pistol og ranet en lokal bakeriet")
